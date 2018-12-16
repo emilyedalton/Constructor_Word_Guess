262f4fa290ef999f9f52d5e88e4728d6885c3be5
Emily Dalton <emilydalton@Emilys-MacBook-Air.local>
2018-12-16 14:28:33 -0600

    initial commits

---
+++ letter.js
@@ -0,0 +1,38 @@
+let guess = process.argv[2];
+//This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter
+let letter = function (character, guessed){
+//A string value to store the underlying character for the letter
+
+this.character = a
+
+//A boolean value that stores whether that letter has been guessed yet
+
+this.guessed = false
+
+//A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+
+this.letterguess = function(){
+    if(this.character === guess){
+        return this.character
+    } else{
+        return "_"
+    }
+
+//A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+this.guessConfirm = function (guess){
+
+    if (this.character === guess){
+        this.guessed = true
+    }
+}
+
+}
+
+
+
+guess.letterguess();
+console.log (guess)
+
+
+
+}
