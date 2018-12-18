7b74052e021a600b7934be4b853837428edebfcc
Emily Dalton <emilydalton@Emilys-MacBook-Air.local>
2018-12-18 09:16:34 -0600

    letters pushing into array

--- letter.js
+++ letter.js
@@ -1,6 +1,6 @@
 let guess = process.argv[2];
 //This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter
-var Letter = function (character){
+var Letter = function (character, guessed){
 //A string value to store the underlying character for the letter
 
 this.character = character;
@@ -11,32 +11,37 @@ this.guessed = false;
 
 //A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
 
-this.letterguess = function(guess){
-    if(this.character === guess){
-        // return this.character;
-        console.log (`you have correctly guessed ${this.character}`)
+this.letterguess = function(){
+    if(this.character){
+        return this.character;
 
-    } else{
-        this.character = "_";
-        console.log ("I am the character now " + this.character)
+    } 
+    return "_";
+        // console.log ("I am the character now " + this.character)
     }
 
 //A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
 this.guessConfirm = function(){
+    {
+        if (guess.toLowerCase() === this.character.toLowerCase()) {
+            this.guessed = true;
+            return true;
+        }
 
-    if (this.character === guess){
-        this.guessed = true;
-        console.log(`your guess was ${this.guessed}`)
-    }
+        // Otherwise return false
+        return false;
+    
+}
+}
 }
 
-}
 
-}
+
+
 
 var getLetter = new Letter("a");   
 
-getLetter.letterguess(guess)
+getLetter.letterguess(guess);
 // getLetter.guessConfirm(guess)
 console.log(getLetter);
 
--- word.js
+++ word.js
@@ -1,26 +1,29 @@
 var Letter = require("./letter.js");
 
-let word = "mouse";
+let word = "red";
 
 ///Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
 
 //An array of `new` Letter objects representing the letters of the underlying word
-let Word = function (){
+let Word = function(word){
 this.underlyingLett = []
-for (var i = 0; i < word.length; i++) {
+ for (var i = 0; i < word.length; i++) {
     this.underlyingLett[i] = "_";
+    this.underlyingLett.push(new Letter(word));
+
 }
 console.log (" I am the array" + this.underlyingLett);
+
     //A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
 
-    this.addLetter = function(word) {
-        this.underlyingLett.push(new Letter(word));
+    // this.addLetter = function(word) {
+    //     this.underlyingLett.push(new Letter(word));
     };
-  };
+  
 
-  var firstClass = new Word();
+  var firstClass = new Word(word);
 
-  firstClass.addLetter(word);
+//   firstClass.addLetter(word);
   console.log(firstClass);
   
 
